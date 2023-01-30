dd7855438cc4ef1c0c2469f9f455a3c9080e67e0
Shamir Khan <xShamir37@gmail.com>
2023-01-30 15:39:06 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+import cv2
+  
+# initialize the camera
+# If you have multiple camera connected with 
+# current device, assign a value in cam_port 
+# variable according to that
+cam_port = 0
+cam = cv2.VideoCapture(cam_port)
+  
+# reading the input using the camera
+result, image = cam.read()
+  
+# If image will detected without any error, 
+# show result
+if result:
+  
+    # showing result, it take frame name and image 
+    # output
+    cv2.imshow("GeeksForGeeks", image)
+  
+    # saving image in local storage
+    cv2.imwrite("GeeksForGeeks.png", image)
+  
+    # If keyboard interrupt occurs, destroy image 
+    # window
+    cv2.waitKey(0)
+    cv2.destroyWindow("GeeksForGeeks")
+  
+# If captured image is corrupted, moving to else part
+else:
+    print("No image detected. Please! try again")
